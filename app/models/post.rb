d70d4029b9364e7c5ff1a4a68f94df823bcd2a28
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-07-20 16:32:02 +0100

    add status to post, create categories model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
   has_many_attached :photos
   validates :title, :rich_body, presence: true
   has_rich_text :rich_body
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+
+  STATUSES = ["draft", "published"]
+  validates :status, inclusion: {in: STATUSES}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_133659) do
+ActiveRecord::Schema.define(version: 2020_07_20_145257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,12 +46,28 @@ ActiveRecord::Schema.define(version: 2020_07_20_133659) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_categories", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_post_categories_on_category_id"
+    t.index ["post_id"], name: "index_post_categories_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.bigint "owner_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["owner_id"], name: "index_posts_on_owner_id"
   end
 
@@ -68,5 +84,7 @@ ActiveRecord::Schema.define(version: 2020_07_20_133659) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "post_categories", "categories"
+  add_foreign_key "post_categories", "posts"
   add_foreign_key "posts", "users", column: "owner_id"
 end
