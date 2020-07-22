5d9f69a4e4fc45b92e27741b27e838ac7daea462
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-07-22 20:31:16 +0100

    integrate cloudinary in rails/config

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many_attached :photos
-  validates :title, :rich_body, presence: true
+  validates :title, :rich_body, :photos, presence: true
   has_rich_text :rich_body
-  has_many :post_categories
+  has_many :post_categories, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :categories, through: :post_categories
 
