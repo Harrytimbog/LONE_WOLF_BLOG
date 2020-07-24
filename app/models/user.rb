20c63c55da8cda5dc6242a53b0a751d05f0da5fe
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-07-24 14:21:56 +0100

    create chatroom channel and integrate action cable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :owned_posts, foreign_key: :owner_id, class_name: "Post", dependent: :destroy
   has_many :reviews
   has_one_attached :avatar
+  has_many :messages
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
