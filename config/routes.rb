3b5cee2e0ccb054820481c66aa8ca948e3ffc045
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-07-31 11:40:25 +0100

    install and integrate rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'posts#index'
   get "posts/my_posts", to: "posts#my_posts", as: :my_profile
